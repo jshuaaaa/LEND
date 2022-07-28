f2f36d46b6153e96a05e5ce356bc9dfe096181e6
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-07-28 14:18:43 -0400

    added some functionality to lEthtoken.sol

---
+++ contracts/Lendingpool.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity ^0.8.0;
+
+contract LendingPool {
+
+    
+    mapping (address => uint) balances;
+    function withdraw(address from, uint amount) public {}
+    function deposit() public {}
+    function borrow() public {}
+    function repayBorrow() public {}
+}
--- contracts/LethToken.sol
+++ contracts/LethToken.sol
@@ -1,15 +1,21 @@
+// SPDX-License-Identifier: agpl-3.0
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
+contract lEth is ERC20, Ownable {
+    constructor(
+    ) ERC20("LendETH", "lETH") {}
+        
+    function init(
+        address LendingPool
+    ) private onlyOwner() {
 
-contract lEth is ERC20 {
-    constructor() ERC20("LendETH", "lETH") {}
-    address LendingPoolContract;
+    }
 
-    function transferToLendingPool(address _to, uint AmountDeposited) public payable {
-        require(_to == LendingPoolContract);
-        AmountDeposited = msg.value;
-        _mint(msg.sender, AmountDeposited);
+    function redeemFromLendingPool(uint amount) public payable {
+        _burn(msg.sender, amount);
+        
     }
 }
