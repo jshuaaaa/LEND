af14c75c52a74076e3acf8e41385d16e872e75d6
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-08-02 16:33:23 -0400

    added final 2 buttons and fixed up the functionality

--- public/web3app.js
+++ public/web3app.js
@@ -1,29 +1,30 @@
 const depositInput = document.getElementById("deposit-input")
 const borrowInput = document.getElementById("borrow-input")
-
+const repayBorrowInput = document.getElementById("repay-borrow-input")
+const withdrawInput = document.getElementById('withdraw')
 async function onLoad() {
     let balance = await LendingPool.methods.getDepositAmountOfUser(account).call()
     balance = balance/10**18
-    document.getElementById("user-weth").textContent += balance
+    document.getElementById("user-weth").textContent = balance
 
     let borrowBalance = await LendingPool.methods.getBorrowAmountOfUser(account).call()
     borrowBalance = borrowBalance/10**18
-    document.getElementById("user-borrow").textContent += borrowBalance
+    document.getElementById("user-borrow").textContent = borrowBalance
 
     wEth("run")
 
 
     let borrowContract = await LendingPool.methods.getBorrowAmountOfUser(LendingPoolAddress).call()
     borrowContract = borrowContract/10**18
-    document.getElementById("borrow-contract").textContent += borrowContract
+    document.getElementById("borrow-contract").textContent = borrowContract
 
     let wethDeposited = await LendingPool.methods.getDepositAmountOfUser(LendingPoolAddress).call()
     wethDeposited = wethDeposited/10**18
-    document.getElementById("weth-deposited").textContent += wethDeposited
+    document.getElementById("weth-deposited").textContent = wethDeposited
 
     let leftToBorrow = wethDeposited * 0.7
     leftToBorrow = leftToBorrow - borrowBalance 
-    document.getElementById("leftto-borrow").textContent += leftToBorrow
+    document.getElementById("leftto-borrow").textContent = leftToBorrow
 }
 
 
@@ -55,6 +56,7 @@ document.getElementById('deposit').addEventListener("click", async function() {
         setTimeout(async () => {
             let txn = await LendingPool.methods.deposit(depositInputVal, lEthAddress).send({from:account})
           }, "15000")
+          onLoad()
         
     })
 
@@ -65,6 +67,26 @@ document.getElementById('borrow').addEventListener("click", async function() {
     let borrowInputVal = borrowInput.value * 10 ** 18
     borrowInputVal = borrowInputVal.toString()
     let txn = await LendingPool.methods.borrow(borrowInputVal).send({from: account})
+    onLoad()
+})
+
+document.getElementById('repay-borrow').addEventListener("click", async function() {
+    let repayBorrowInputVal = repayBorrowInput.value * 10 ** 18
+    repayBorrowInputVal = repayBorrowInputVal.toString()
+    let approval = await wEth(account, repayBorrowInputVal)
+    setTimeout(async () => {
+    let txn = await LendingPool.methods.repayBorrow(repayBorrowInputVal).send({from: account})
+    }, "15000")
+    onLoad()
+})
+
+document.getElementById('withdraw').addEventListener("click", async function() {
+    let withdrawInputVal = withdrawInput.value * 10 ** 18
+    withdrawInputVal = withdrawInputVal.toString()
+    let txn = await LendingPool.methods.withdraw(withdrawInputVal, lEthAddress).send({from: account})
+
+          
+    onLoad()
 })
 
 
